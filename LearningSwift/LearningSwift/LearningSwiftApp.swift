12a4211fc45c0621d978617d1f892ae6a15d31d5
Robert Ullrich <roul76@googlemail.com>
2022-02-26 13:43:51 +0100

    feat: initialize the blank xcode project

---
+++ LearningSwift/LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Robert Ullrich on 26.02.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
